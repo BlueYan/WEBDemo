ec9b93af1ddb4013ec509a43f7b81381a2e40c31
Mark Yan <MarkYan@163.com>
2017-06-08 09:50:23 +0800

    提交项目结构

---
+++ src/main/java/com/mark/servlet/HelloServlet.java
@@ -0,0 +1,33 @@
+package com.mark.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2017/6/2.
+ */
+public class HelloServlet implements Servlet {
+
+	public void init(ServletConfig servletConfig) throws ServletException {
+
+	}
+
+	public ServletConfig getServletConfig() {
+		return null;
+	}
+
+	public void service(ServletRequest servletRequest, ServletResponse servletResponse)
+			throws ServletException, IOException {
+
+		System.out.println("HelloServlet.service()");
+
+	}
+
+	public String getServletInfo() {
+		return null;
+	}
+
+	public void destroy() {
+
+	}
+}
